75165548b935ec38da0155b6bf5644e77d6c4cfc
intershieldppf <interppff@gmail.com>
2026-08-19 12:37:17 -0300

    Use real photos in Black Piano finish cards

--- src/app/black-piano/layout.tsx
+++ src/app/black-piano/layout.tsx
@@ -19,6 +19,34 @@ export default function BlackPianoLayout({ children }: { children: ReactNode }) 
         .black-piano-page main > section:first-child > div > div:nth-child(2) > div {
           display: none !important;
         }
+
+        .black-piano-page main > section:nth-child(2) > div > div > article > div > article:nth-child(1) > div:first-child {
+          background-image: url('/black-piano-card-gloss.webp') !important;
+          background-size: cover !important;
+          background-position: center center !important;
+          background-repeat: no-repeat !important;
+          background-color: #f8fafc !important;
+        }
+
+        .black-piano-page main > section:nth-child(2) > div > div > article > div > article:nth-child(2) > div:first-child {
+          background-image: url('/black-piano-card-matte.webp') !important;
+          background-size: cover !important;
+          background-position: center center !important;
+          background-repeat: no-repeat !important;
+          background-color: #18181b !important;
+        }
+
+        .black-piano-page main > section:nth-child(2) > div > div > article > div > article:nth-child(3) > div:first-child {
+          background-image: url('/black-piano-card-carbon.webp') !important;
+          background-size: cover !important;
+          background-position: center center !important;
+          background-repeat: no-repeat !important;
+          background-color: #0f172a !important;
+        }
+
+        .black-piano-page main > section:nth-child(2) > div > div > article > div > article > div:first-child > div {
+          display: none !important;
+        }
       `}</style>
       {children}
     </div>
