43bdf19517ea759cf16272d31c28fa91201f92f5
intershieldppf <interppff@gmail.com>
2026-08-20 18:13:11 -0300

    Add interior protection video section
    Add a branded home-page section with the supplied interior PPF application video, accessible audio controls, responsive layout and catalog navigation.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { FeatureSection } from "@/components/FeatureSection";
 import { Header } from "@/components/layout/Header";
 import { HeroSection } from "@/components/HeroSection";
 import { InfoSection } from "@/components/InfoSection";
+import { InteriorProtectionVideoSection } from "@/components/InteriorProtectionVideoSection";
 import { MarketplaceSection } from "@/components/MarketplaceSection";
 import { SupportSection } from "@/components/SupportSection";
 import { VehicleBar } from "@/components/search/VehicleBar";
@@ -30,8 +31,9 @@ export default function Home() {
 
         <VehicleBar />
         <HeroSection />
+        <InteriorProtectionVideoSection />
 
-        <div className="mx-auto max-w-7xl space-y-20 px-6 py-16 sm:px-8 lg:px-10 lg:py-24">
+        <div className="mx-auto max-w-7xl space-y-20 px-6 pb-16 sm:px-8 lg:px-10 lg:pb-24">
           <InfoSection />
           <FeatureSection />
           <AboutSection />
